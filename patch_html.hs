56bdda4305442785a39897c703f00df93103b45b
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2012-07-02 16:47:29 +0200

    refactored patch_html.hs

--- patch_html.hs
+++ patch_html.hs
@@ -2,23 +2,23 @@
 
 import System.IO
 import Text.HTML.TagSoup
--- import Text.HTML.TagSoup.Tree
-
 
 removeScript :: [Tag String] -> [Tag String]
-removeScript ((TagOpen "script" _):tags) = removeScript tags
-removeScript ((TagClose "script"):tags) = removeScript tags
-removeScript (other:tags) = other:(removeScript tags)
-removeScript [] = []
+removeScript tags = case tags of
+    (TagOpen "script" _):_tags -> removeScript _tags
+    (TagClose "script"):_tags  -> removeScript _tags
+    (other:_tags)              -> other:(removeScript _tags)
+    []                         -> []
 
 insert :: [Tag String] -> [Tag String] -> [Tag String]
-insert ((TagOpen "head" attrs):tags) extra = (TagOpen "head" attrs):extra ++ tags
-insert (tag:tags) extra = tag:(insert tags extra)
-insert [] extra = []
+insert elts tags = case tags of
+     (TagOpen "head" attrs):_tags -> (TagOpen "head" attrs):elts ++ _tags
+     tag:_tags                    -> tag:(insert elts _tags)
+     _                            -> []
 
 main = do
     mathjax <- readFile "mathjax.html"
     let scripts = parseTags mathjax
     html <- getContents
-    let tags = insert (removeScript (parseTags html)) scripts
+    let tags = (insert scripts . removeScript . parseTags) html
     putStr (renderTags tags)
